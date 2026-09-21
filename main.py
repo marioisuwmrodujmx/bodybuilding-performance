30d24f4ddb67e49f7e27e22fa716c3def3a66eb8
Alonso Mayer <alonsomm1705@gmail.com>
2026-09-22 00:03:48 +0200

    Add training volume calculation

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Bodybuilding Performance")
-print("My first bodybuilding data project")
+exercise = input("ejercicio: ")
+weight = float(input("peso (kg): "))
+reps = float(input("repeticiones: "))
+print("Has registrado: ")
+print(exercise, "-", weight, "kg x", reps, "reps")
+volume = weight * reps
+print("volumen de entrenamiento: ", volume)
